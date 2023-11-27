2e4c82b1dab7a92d1e3c4c0a7ba5666c23fdc6e7
Niranjankumar7 <niranjankumararkalgud@outlook.com>
2023-11-27 19:05:23 +0530

    Added multiple stocks and generates csv for each stock in different folder

--- README.md
+++ README.md
@@ -2,18 +2,19 @@
 
 Personal Stock market assistant.
 
+The stop loss has been updated to the day of open when it gave breakout/breakdown.
+
 This project helping me to learn python and also make an stock filtration list using python
 
-
 Future steps for this project
-Make as many back testing setup as possibile. 
+Make as many back testing setup as possibile.
 Dynamically use API and fetch data of a stock.
-For insider candle setup - 
+For insider candle setup -
 Make 3 to 20 days insider candle pattern stock.
 
 Can think of many strategies like these and apply them using the code to backtest the data and
-then also use as a stock screener where it fetches data from API to list me all the stocks which comes in each setup with 
-it's probability of succession. 
+then also use as a stock screener where it fetches data from API to list me all the stocks which comes in each setup with
+it's probability of succession.
 
 Add more creteria to see if volume helps.
 Find stocks which are side ways and stuck in a zone.
@@ -22,17 +23,6 @@ Bollinger band setup and how many days it stays inside it for the maximum.
 
 Figure out a way to fetch data of options and it's premiun so that we can judge upon the decay
 Figure out a way to perfectly calcuate decay using IV and other factors.
- 
 
 Future Future ->
 Figure out how you can teach this if a stock is good or not. Then based upon technicals and fundamental analysis let it give out the suggested stock which can taken.
-
-
-
-
-
-
-
-
-
-
--- new_an.py
+++ new_an.py
@@ -1,60 +1,104 @@
 import yfinance as yf
 import pandas as pd
+import os
 
-# Fetch the stock data
-stock_symbol = 'ICICIBANK.NS'
+
+def backtest_stock(stock_symbol, start_date, end_date, output_folder):
+    # Fetch the stock data
+    df = yf.download(stock_symbol, start=start_date, end=end_date)
+    df.reset_index(inplace=True)
+
+    # Initialize a list to hold the trade details
+    trades = []
+    total_trades = 0
+    profitable_trades = 0
+    loss_trades = 0
+
+    # Loop through the DataFrame to check for a 3-day inside pattern
+    for i in range(len(df) - 6):
+        # Define the high and low of the first day
+        first_day_high = df.loc[i, 'High']
+        first_day_low = df.loc[i, 'Low']
+
+        # Check if the second and third days are inside days
+        if all(df.loc[i+1:i+2, 'High'] <= first_day_high) and all(df.loc[i+1:i+2, 'Low'] >= first_day_low):
+            # Check if the fourth day closes outside the first day's range
+            fourth_day_close = df.loc[i+3, 'Close']
+            if fourth_day_close > first_day_high or fourth_day_close < first_day_low:
+                # A trade has been triggered
+                entry_date = df.loc[i+3, 'Date']
+                entry = fourth_day_close
+
+                # Using the open value of the breakout day as stop loss
+                stop_loss = df.loc[i+3, 'Open']
+
+                # Calculating the target based on the new stop loss
+                target = entry - \
+                    (2 * (stop_loss - entry)) if fourth_day_close < first_day_low else entry + \
+                    (2 * (entry - stop_loss))
+
+                # Record the 7th day closing value
+                seventh_day_close = df.loc[i+6, 'Close']
+
+                # Determine if the trade is in profit or loss on the 7th day
+                trade_result = "Profit" if (fourth_day_close < first_day_low and seventh_day_close < entry) or (
+                    fourth_day_close > first_day_high and seventh_day_close > entry) else "Loss"
+                profit_or_loss_amount = seventh_day_close - entry
+                total_trades += 1
+
+                # Increment profitable or loss trades
+                if trade_result == "Profit":
+                    profitable_trades += 1
+                else:
+                    loss_trades += 1
+
+                # Calculate win percentage
+                win_percentage = (profitable_trades / total_trades * 100)
+
+                # Add the trade details to the list
+                trades.append({
+                    'Entry_Date': entry_date,
+                    'Entry': entry,
+                    'Stop_Loss': stop_loss,
+                    'Target': target,
+                    'Seventh_Day_Close': seventh_day_close,
+                    'Trade_Result': trade_result,
+                    'Profit_or_Loss_Amount': profit_or_loss_amount,
+                    'Total Trades': total_trades,
+                    'Profitable Trades': profitable_trades,
+                    'Loss Trades': loss_trades,
+                    'Win Percentage': win_percentage
+                })
+
+    # Convert the trades list to a DataFrame
+    trades_df = pd.DataFrame(trades)
+
+    # Save the trades DataFrame to a CSV file
+    output_file = os.path.join(output_folder, f'{stock_symbol}_trades.csv')
+    trades_df.to_csv(output_file, index=False)
+
+    print(f"Trades data saved to {output_file}")
+
+
+def backtest_multiple_stocks(stock_symbols, start_date, end_date, output_folder):
+    for stock_symbol in stock_symbols:
+        backtest_stock(stock_symbol, start_date, end_date, output_folder)
+
+
+# List of stock symbols
+stock_symbols = ['ICICIBANK.NS', 'RELIANCE.NS', 'BAJAJ-AUTO.NS', 'MARUTI.NS', 'LTIM.NS',
+                 'TITAN.NS', 'ULTRACEMCO.NS', 'NTPC.NS', 'TATACONSUM.NS', 'KOTAKBANK.NS', 'HDFCLIFE.NS',
+                 'COALINDIA.NS', 'TATASTEEL.NS', 'BAJFINANCE.NS', 'BAJAJFINSV.NS', 'ITC.NS',
+                 'BHARATIARTL.NS', 'HEROMOTOCO.NS', 'INDUSINDBK.NS', 'NESTLEIND.NS',
+                 'TECHM.NS', 'ONGC.NS', 'BRITANNIA.NS', 'HINDALCO.NS', 'TCS.NS', 'APOLLOHOSP.NS',
+                 'WIPRO.NS', 'CIPLA.NS', 'ADANIENT.NS', 'MM.NS']
+
+# Specify the start and end dates
 start_date = '2018-01-01'
 end_date = '2023-11-20'
-df = yf.download(stock_symbol, start=start_date, end=end_date)
-df.reset_index(inplace=True)
 
-# Initialize a list to hold the trade details
-trades = []
+# Specify the output folder
+output_folder = 'Backtest_Results'
 
-# Loop through the DataFrame to check for a 3-day inside pattern
-for i in range(len(df) - 6):
-    # Define the high and low of the first day
-    first_day_high = df.loc[i, 'High']
-    first_day_low = df.loc[i, 'Low']
-
-    # Check if the second and third days are inside days
-    if all(df.loc[i+1:i+2, 'High'] <= first_day_high) and all(df.loc[i+1:i+2, 'Low'] >= first_day_low):
-        # Check if the fourth day closes outside the first day's range
-        fourth_day_close = df.loc[i+3, 'Close']
-        if fourth_day_close > first_day_high or fourth_day_close < first_day_low:
-            # A trade has been triggered
-            entry_date = df.loc[i+3, 'Date']
-            entry = fourth_day_close
-
-            # Using the open value of the breakout day as stop loss
-            stop_loss = df.loc[i+3, 'Open']
-
-            # Calculating the target based on the new stop loss
-            target = entry - (2 * (stop_loss - entry)) if fourth_day_close < first_day_low else entry + (2 * (entry - stop_loss))
-
-            # Record the 7th day closing value
-            seventh_day_close = df.loc[i+6, 'Close']
-
-            # Determine if the trade is in profit or loss on the 7th day
-            trade_result = "Profit" if (fourth_day_close < first_day_low and seventh_day_close < entry) or (fourth_day_close > first_day_high and seventh_day_close > entry) else "Loss"
-            profit_or_loss_amount = seventh_day_close - entry
-
-            # Add the trade details to the list
-            trades.append({
-                'Entry_Date': entry_date,
-                'Entry': entry,
-                'Stop_Loss': stop_loss,
-                'Target': target,
-                'Seventh_Day_Close': seventh_day_close,
-                '1:2': trade_result,
-                'Result on 7th Day': profit_or_loss_amount
-            })
-
-# Convert the trades list to a DataFrame
-trades_df = pd.DataFrame(trades)
-
-# Save the trades DataFrame to a CSV file
-output_file = 'trades_output_modified.csv'
-trades_df.to_csv(output_file, index=False)
-
-print(f"Trades data saved to {output_file}")
+# Backtest the stocks
+backtest_multiple_stocks(stock_symbols, start_date, end_date, output_folder)
